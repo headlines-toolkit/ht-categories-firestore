24ea42355dd472275098413d49db351caa605d47
fulleni <fulleni@hotmail.com>
2025-04-02 06:32:37 +0100

    chore: initial commit for ht_categories_firestore
    - Added basic library structure
    - Integrated ht_categories_client
    - Setup CI and issue templates

---
+++ lib/src/ht_categories_firestore.dart
@@ -0,0 +1,7 @@
+/// {@template ht_categories_firestore}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtCategoriesFirestore {
+  /// {@macro ht_categories_firestore}
+  const HtCategoriesFirestore();
+}
---
+++ test/src/ht_categories_firestore_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ht_categories_firestore/ht_categories_firestore.dart';
+
+void main() {
+  group('HtCategoriesFirestore', () {
+    test('can be instantiated', () {
+      expect(HtCategoriesFirestore(), isNotNull);
+    });
+  });
+}
