c6929eb543e39ed543fa81926679e12b159879ae
fulleni <fulleni@hotmail.com>
2025-04-02 07:54:08 +0100

    docs: Add Firestore categories library description

--- lib/ht_categories_firestore.dart
+++ lib/ht_categories_firestore.dart
@@ -1,4 +1,5 @@
-/// A Very Good Project created by Very Good CLI.
+/// A Firestore implementation of the HtCategoriesClient interface, providing
+/// CRUD operations for categories using Cloud Firestore.
 library;
 
 export 'src/ht_categories_firestore.dart';
